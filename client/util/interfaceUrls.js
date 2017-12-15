41f12ad08070a85e0946ddf0c185079a271a0de1
chengherui <chengherui@meituan.com>
2017-12-15 17:23:16 +0800

    up

---
+++ client/util/interfaceUrls.js
@@ -0,0 +1,18 @@
+var prefix = 'https://jtx5hmyx.qcloud.la/weapp/';
+
+module.exports = {
+    //helpList
+    "searchServiceNeeded":prefix + "searchServiceNeeded",
+    //myPage
+    "getUserInfo":prefix + "getUserInfo",
+    //helpDetail
+    "acceptRequest":prefix + "acceptRequest",
+    //helpInput
+    "postNeed":prefix + "postNeed",
+    //myToHelp
+    "searchMyService":prefix + "searchMyService",
+    //myGetHelp
+    "searchMyNeed":prefix + "searchMyNeed",
+    "confirmService":prefix + "confirmService",
+    "confirmServiceComplete":prefix + "confirmServiceComplete"
+};
